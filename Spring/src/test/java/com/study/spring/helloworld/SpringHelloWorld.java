753e5cf035fedc15d84b8af80fc48cb3bcda399b
Impler <704090860@qq.com>
2015-09-27 10:18:45 +0800

    avoid warning

--- Spring/src/test/java/com/study/spring/helloworld/SpringHelloWorld.java
+++ Spring/src/test/java/com/study/spring/helloworld/SpringHelloWorld.java
@@ -2,7 +2,6 @@ package com.study.spring.helloworld;
 
 
 import org.junit.Test;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.study.spring.helloworld.dao.IUserDao;
@@ -11,9 +10,10 @@ public class SpringHelloWorld {
 
 	@Test
 	public void testHello() {
-		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+		ClassPathXmlApplicationContext  ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
 		IUserDao userDao = (IUserDao) ctx.getBean("userDao");
 		userDao.save();
+		ctx.close();
 	}
 
 }
