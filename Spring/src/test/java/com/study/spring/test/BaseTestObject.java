fa09ae075ef960440d4d8911f79aa849932bfc34
Impler <704090860@qq.com>
2015-11-20 17:12:50 +0800

    annotation based configuration

--- Spring/src/test/java/com/study/spring/test/BaseTestObject.java
+++ Spring/src/test/java/com/study/spring/test/BaseTestObject.java
@@ -2,17 +2,21 @@ package com.study.spring.test;
 
 import org.junit.After;
 import org.junit.Before;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class BaseTestObject {
 	private ClassPathXmlApplicationContext ctx;
+	private AnnotationConfigApplicationContext aCtx;
 	@Before
 	public void prepare(){
 		ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+		aCtx = new AnnotationConfigApplicationContext("anoAppCtx.xml");
 	}
 	@After
 	public void close(){
 		ctx.close();
+		aCtx.close();
 	}
 	
 	public ClassPathXmlApplicationContext getContext(){
@@ -21,6 +25,14 @@ public class BaseTestObject {
 	
 	public Object getBean(String beanName){
 		return ctx.getBean(beanName);
+	}
+	
+	public AnnotationConfigApplicationContext getAnotationContext(){
+		return aCtx;
+	}
+	
+	public Object getAnoBean(String beanName){
+		return aCtx.getBean(beanName);
 	}
 	
 }
