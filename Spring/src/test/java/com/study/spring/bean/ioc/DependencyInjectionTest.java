df868e5c32077263b7ecc5a625f3fe83e014d488
Impler <704090860@qq.com>
2015-11-20 13:59:27 +0800

    configuration details of beans

--- Spring/src/test/java/com/study/spring/bean/ioc/DependencyInjectionTest.java
+++ Spring/src/test/java/com/study/spring/bean/ioc/DependencyInjectionTest.java
@@ -8,59 +8,77 @@ public class DependencyInjectionTest extends BaseTestObject{
 	@Test
 	//setter injection
 	public void testSetterInjection() {
-		DependencyInjection di = (DependencyInjection) super.getBean("SetterDIBean");
+		DependencyInjection di = (DependencyInjection) super.getBean("setterDIBean");
 		System.out.print(di.getSetterBasedUser());
 	}
 	
 	@Test
 	//injection by argument type
 	public void testConstInjection1(){
-		DependencyInjection di = (DependencyInjection) super.getBean("ConstDIBean1");
+		DependencyInjection di = (DependencyInjection) super.getBean("constDIBean1");
 		System.out.print(di.getConstBasedUser());
 	}
 	
 	@Test
 	//arguments with the same type
 	public void testConstInjection2(){
-		DependencyInjection di = (DependencyInjection) super.getBean("ConstDIBean2");
+		DependencyInjection di = (DependencyInjection) super.getBean("constDIBean2");
 		System.out.print(di.getConstBasedUser());
 	}
 	@Test
 	//arguments with the same type, use index to avoid ambiguity 
 	public void testConstInjection3(){
-		DependencyInjection di = (DependencyInjection) super.getBean("ConstDIBean3");
+		DependencyInjection di = (DependencyInjection) super.getBean("constDIBean3");
 		System.out.print(di.getConstBasedUser());
 	}
 	@Test
 	//more complication, use index and type together 
 	public void testConstInjection4(){
-		DependencyInjection di = (DependencyInjection) super.getBean("ConstDIBean4");
+		DependencyInjection di = (DependencyInjection) super.getBean("constDIBean4");
 		System.out.print(di.getConstBasedUser());
 	}
 	
 	@Test
 	//non static factory method injection
 	public void testNonStaticFactoryInjection(){
-		User u = (User) super.getBean("FactoryDIBean");
+		User u = (User) super.getBean("factoryDIBean");
 		System.out.print(u);
 	}
 	
 	@Test
 	//static factory method injection
 	public void testStaticFactoryInjection(){
-		User u = (User) super.getBean("StaticFactoryDIBean");
+		User u = (User) super.getBean("staticFactoryDIBean");
 		System.out.print(u);
 	}
 	
 	@Test
 	public void testInjectPB2SBBaseOnSpring(){
-		InjectPB2SBBaseOnSpring bean = (InjectPB2SBBaseOnSpring) super.getBean("SingletonBean1");
+		InjectPB2SBBaseOnSpring bean = (InjectPB2SBBaseOnSpring) super.getBean("singletonBean1");
 		System.out.print(bean.getPrototypeUser() == bean.getPrototypeUser());
 	}
 	
 	@Test
 	public void testInjectPB2SBBaseOnDynamicProxy(){
-		InjectPB2SBBaseOnDynamicProxy bean = (InjectPB2SBBaseOnDynamicProxy) super.getBean("SingletonBean2");
+		InjectPB2SBBaseOnDynamicProxy bean = (InjectPB2SBBaseOnDynamicProxy) super.getBean("singletonBean2");
 		System.out.print(bean.getUser() == bean.getUser());
+	}
+	
+	@Test
+	public void testIdref(){
+		User u = (User) super.getBean("idrefBean");
+		System.out.println(u);
+	}
+	
+	@Test
+	public void testInnerBean(){
+		DependencyInjection di = (DependencyInjection) super.getBean("innerBean");
+		System.out.println(di.getSetterBasedUser());
+	}
+	
+	@Test
+	public void testDependsOn(){
+		DependencyInjection di = (DependencyInjection) super.getBean("dependsOnBean");
+		System.out.println(di.getSetterBasedUser());
 	}
 }
