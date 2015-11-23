082bd83eefd97f780c7dbac40f20c8e0bd12f627
Impler <704090860@qq.com>
2015-11-23 13:31:12 +0800

    test annotation with no xml configuration

--- Spring/src/test/java/com/study/spring/annotation/TestAnnotation.java
+++ Spring/src/test/java/com/study/spring/annotation/TestAnnotation.java
@@ -2,10 +2,13 @@ package com.study.spring.annotation;
 
 
 import org.junit.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
 import com.study.spring.base.domain.User;
 import com.study.spring.test.BaseTestObject;
 
+@ComponentScan("com.study.spring.annotation")
 public class TestAnnotation extends BaseTestObject{
 
 	@Test
@@ -15,4 +18,12 @@ public class TestAnnotation extends BaseTestObject{
 		manager.register(u);
 	}
 	
+	@Test
+	public void testAnnotationWithAnnoContext(){
+		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(this.getClass());
+		UserManager manager = (UserManager) ctx.getBean("userManager");
+		User u = new User("Tom", 24, 'M', "BeiJing");
+		manager.register(u);
+		ctx.close();
+	}
 }
