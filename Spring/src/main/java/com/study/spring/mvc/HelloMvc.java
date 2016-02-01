1af46c8c88a0028cd9eabcf5d42a6566e3d8dc58
Impler <704090860@qq.com>
2016-02-01 22:29:26 +0800

    spring mvc

---
+++ Spring/src/main/java/com/study/spring/mvc/HelloMvc.java
@@ -0,0 +1,17 @@
+package com.study.spring.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloMvc {
+	
+	@RequestMapping("/mvc/hello")
+	public ModelAndView hello(){
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("mvc/hello.jsp");
+		mv.addObject("hello", "Hello Spring MVC");
+		return mv;
+	}
+}
