eee49404dabf89bf47c431cea4a07021d160c91d
Impler <704090860@qq.com>
2017-03-01 17:08:25 +0800

    spring bean naming alias

---
+++ Spring/src/test/java/com/study/spring/_01core/_01ioc/_02beannaming/BeanNamingAliasTest.java
@@ -0,0 +1,35 @@
+package com.study.spring._01core._01ioc._02beannaming;
+
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.study.spring._01core._01ioc._01baseconfig.BusinessService;
+
+public class BeanNamingAliasTest {
+
+	@Test
+	public void test() {
+		String[] configFiles = {"config/01core/01ioc/02beannaming/beans-base-alias.xml"
+				, "config/01core/01ioc/02beannaming/beans-base-alias-module-a.xml"
+				, "config/01core/01ioc/02beannaming/beans-base-alias-module-b.xml"
+		};
+		
+		ClassPathXmlApplicationContext context = 
+				new ClassPathXmlApplicationContext(configFiles);
+		
+		
+		String[] alias = context.getAliases("businessDao");
+		System.out.println("******businessDao all aliases are below:");
+		for(String a : alias){
+			System.out.println("alias:" + a);
+		}
+		
+		BusinessService serviceA = (BusinessService) context.getBean("businessServiceA");
+		BusinessService serviceB = (BusinessService) context.getBean("businessServiceB");
+		System.out.println("******is refed the same bean by alias: ");
+		System.out.println(serviceA.getBusinessDao() == serviceB.getBusinessDao());
+		
+		context.close();
+	}
+
+}
