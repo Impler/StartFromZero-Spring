7aba6bdc8913eda079c9ffdf9bdd65e58bc20774
Impler <704090860@qq.com>
2017-03-06 11:31:59 +0800

    spring method injection

---
+++ Spring/src/main/java/com/study/spring/_01core/_01ioc/_04dependency_injection/method_injection/PrototypeBeanBar.java
@@ -0,0 +1,10 @@
+package com.study.spring._01core._01ioc._04dependency_injection.method_injection;
+
+/**
+ * a test prototype bean
+ * @author impler
+ * @date 2017-03-06
+ */
+public class PrototypeBeanBar {
+
+}
