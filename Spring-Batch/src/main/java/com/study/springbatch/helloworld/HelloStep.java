a233e6af0ceb8deac61f0ce1760ea740a7fefbe6
Impler <704090860@qq.com>
2017-05-18 15:45:53 +0800

    spring batch tutorial

---
+++ Spring-Batch/src/main/java/com/study/springbatch/helloworld/HelloStep.java
@@ -0,0 +1,17 @@
+package com.study.springbatch.helloworld;
+
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+
+public class HelloStep implements Tasklet {
+
+	@Override
+	public RepeatStatus execute(StepContribution contribution,
+			ChunkContext chunkContext) throws Exception {
+		System.out.println("Hello ");
+		return RepeatStatus.FINISHED;
+	}
+
+}
