176400ff300a7a82b3cb2589460c0d04150d16d4
Impler <704090860@qq.com>
2017-11-24 09:43:17 +0800

    spring review -- spring factorybean

---
+++ SpringPOM/Spring-Core/Spring-Core-IOC/Spring-Core-IOC-06ExtensionPoints/src/main/java/com/study/spring/core/ioc/extendion_points/factorybean/SqlSessionFactoryBean.java
@@ -0,0 +1,10 @@
+package com.study.spring.core.ioc.extendion_points.factorybean;
+
+/**
+ * @see org.mybatis.spring.SqlSessionFactoryBean
+ * @author impler
+ * @date 2017年11月24日
+ */
+public class SqlSessionFactoryBean {
+
+}
